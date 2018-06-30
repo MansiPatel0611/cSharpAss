f9588089977fa7810f9ffe28a8086bd0a0ffe964
Mansi Patel <Mansi@promact.com>
2018-06-30 16:05:47 +0530

    added assignment

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
-var fname = document.getElementById('f_name');
-var lname= document.getElementById('l_name');
-var email=document.getElementById('eid');
-var address=document.getElementById('address');
-var uname=document.getElementById('u_name');
-var pass=document.getElementById('pass');
-var gender=document.getElementById('gender').value;
-var gen;
-if(gender =='male'){
-    gen = document.getElementById('male').value;
-}
-else
+class employee
 {
-    gen = document.getElementById('female').value;
-
+	constructor()
+	{
+this.fname = document.getElementById('f_name').value;
+this.lname= document.getElementById('l_name').value;
+this.email=document.getElementById('eid').value;
+this.address=document.getElementById('address').value;
+this.uname=document.getElementById('u_name').value;
+this.pass=document.getElementById('pass').value;
+var gender=document.getElementsByName('rgender');
+for(var i=0;i<gender.length;i++)
+{
+if(gender[i].checked){
+    this.gen = gender[i].value;
 }
 
-var qualification=document.getElementById('qualification');
-var experience=document.getElementById('experience');
+}
+this.qualification=document.getElementById('qualification').value;
+this.experience=document.getElementById('experience').value;
 
  var checkboxes = document.getElementsByName('codelist');
-  var checkboxesChecked = [];
+  this.checkboxesChecked = [];
   for (var i=0; i<checkboxes.length; i++) {
      if (checkboxes[i].checked==true) {
-        checkboxesChecked.push(checkboxes[i].value);
+        this.checkboxesChecked.push(checkboxes[i].value);
      }
   }
+	}
+	}
+
 
 document.querySelector('form.myform').addEventListener('submit', function (e) {
 
     //prevent the normal submission of the form
     e.preventDefault();
-
-    console.log("First name: "+fname.value);
-	console.log("Last name: "+lname.value);    
-	console.log("Email: "+email.value);
-	console.log("Address: "+address.value);
-	console.log("User name: "+uname.value);
-	console.log("Password: "+pass.value);
-	console.log("Gender: "+gen);
-	console.log("Qualification: "+qualification.value);
-	console.log("Experience: "+experience.value);
-	console.log("Coding Languages: "+checkboxesChecked);
+var emp=new employee();
+	console.log(emp);
 });
